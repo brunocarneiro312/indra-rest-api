e418260991a56559cf3bc2651df7c39ad5e516fe
bruno.carneiro <bruno.carneiro@gmail.com>
2019-05-01 18:50:54 -0300

    mapeando entidades

--- src/main/java/br/com/brunocarneiro/desafioindra/modelo/Endereco.java
+++ src/main/java/br/com/brunocarneiro/desafioindra/modelo/Endereco.java
@@ -1,38 +1,47 @@
 package br.com.brunocarneiro.desafioindra.modelo;
 
-import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "ENDERECO")
 public class Endereco implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    public Endereco() {
-
-    }
-
-    public Endereco(Long id,
-                    String cep,
-                    String logradouro,
-                    String bairro,
-                    String cidade,
-                    String complemento,
-                    String uf) {
-
-        this.id = id;
-        this.cep = cep;
-        this.logradouro = logradouro;
-        this.bairro = bairro;
-        this.cidade = cidade;
-        this.uf = uf;
-    }
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_endereco")
+    @SequenceGenerator(name = "seq_endereco", sequenceName = "seq_endereco", initialValue = 4)
+    @Column(name = "ID_ENDERECO")
     private Long id;
+
+    @Column(name = "CEP", nullable = false)
     private String cep;
+
+    @Column(name = "LOGRADOURO", nullable = false)
     private String logradouro;
+
+    @Column(name = "BAIRRO", nullable = false)
     private String bairro;
+
+    @Column(name = "CIDADE", nullable = false)
     private String cidade;
+
+    @Column(name = "UF", nullable = false)
     private String uf;
+
+    @Column(name = "COMPLEMENTO")
     private String complemento;
-    private Pessoa pessoa;
+
+    @OneToMany(mappedBy = "endereco")
+    private List<Pessoa> pessoas;
 
 }
--- src/main/java/br/com/brunocarneiro/desafioindra/modelo/Pessoa.java
+++ src/main/java/br/com/brunocarneiro/desafioindra/modelo/Pessoa.java
@@ -1,15 +1,45 @@
 package br.com.brunocarneiro.desafioindra.modelo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "PESSOA")
 public class Pessoa implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_pessoa")
+    @SequenceGenerator(name = "seq_pessoa", sequenceName = "seq_pessoa", initialValue = 4)
+    @Column(name = "ID_PESSOA")
     private Long id;
+
+    @Column(name = "NOME", nullable = false)
+    @Size(min = 2, max = 50)
     private String nome;
+
+    @Column(name = "SOBRENOME", nullable = false)
+    @Size(min = 2, max = 100)
     private String sobrenome;
+
+    @Column(name = "EMAIL", unique = true, nullable = false)
+    @Email
     private String email;
+
+    @ManyToOne
+    @JoinTable(name = "PESSOA_ENDERECO",
+            joinColumns = @JoinColumn(name = "ID_PESSOA", referencedColumnName = "ID_PESSOA"),
+            inverseJoinColumns = @JoinColumn(name = "ID_ENDERECO", referencedColumnName = "ID_ENDERECO"))
     private Endereco endereco;
 
 }
