7ec3631cce90b16ff52454488a8bbd1f0a85ea80
bruno.carneiro <bruno.carneiro@gmail.com>
2019-05-01 17:57:57 -0300

    setup

---
+++ src/main/java/br/com/brunocarneiro/desafioindra/DesafioIndraApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunocarneiro.desafioindra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioIndraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioIndraApplication.class, args);
+    }
+
+}
